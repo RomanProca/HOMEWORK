ce919c84fe638d702bc76fdc2a6efebcb2654a91
RomanProca <romansermicioi@gmail.com.com>
2019-07-22 13:11:32 +0300

    The second homework was added.

---
+++ Lection 2/Movie.java
@@ -0,0 +1,15 @@
+public class Movie
+{
+    public static void main(String... args)
+    {
+        System.out.print("'Here's Johnny!'\n");
+        System.out.print("'Here's Johnny!',The Shining, Jack Nicholson, 1980\n");
+        System.out.print("X                      X\n");
+        System.out.print("X                      X\n");
+        System.out.print("X      XXXXXXXXXX      X\n");
+        System.out.print("XXXXX  X        X  XXXXX\n");
+        System.out.print("X   X  X        X  X   X\n");
+        System.out.print("X   X  X        X  X   X\n");
+        System.out.printf("\n");
+    }
+}
