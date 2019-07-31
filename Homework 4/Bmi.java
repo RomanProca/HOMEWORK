1093af563cc0af280668933f2d4f2d91295d0fb0
RomanProca <romansermicioi@gmail.com.com>
2019-07-31 18:32:49 +0300

    -add Homework 4

---
+++ Homework 4/Bmi.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class Bmi{
+    public static void main(String[] args){
+        Scanner input = new Scanner( System.in );
+        double userFeet;
+        double userInch;
+        System.out.println("Insert your height in feet");
+        userFeet = input.nextDouble();
+        System.out.println("Insert your remaining height in Inches");
+        userInch = input.nextDouble();
+        double userPounds;
+        System.out.println("Insert your weight in pounds:");//60kg=132pounds
+        userPounds = input.nextDouble();
+        double inches = userFeet * 12 + userInch;
+        double meters = inches * 0.0254;
+        double kilograms = userPounds / 2.2;
+        double bmi = kilograms / ( meters * meters ) ;
+                System.out.printf("Your height in meters: %.2f \n", meters);
+                System.out.printf("Your weight in kilograms: %.2f\n", kilograms);
+                System.out.printf("Your BMI: %.2f\n",bmi);
+
+    }
+}
---
+++ Homework 4/Height.java
@@ -0,0 +1,18 @@
+import java.io.*;
+class Height {
+    public static void main(String[] args) {
+        double height = 175;
+        Conversion(height);
+    }
+
+    static void Conversion(double height) {
+        double inch = 0.3937 * height;
+        int intPart = (int) inch;
+        System.out.printf("Height in integer inches: %d \n", intPart);
+        double feet = intPart / 12;
+        double rest = intPart - feet * 12;
+        System.out.printf("Height in integer inches converted to feet and inches: %.2f feet and %.2f inch\n", feet, rest);
+
+    }
+}
+
