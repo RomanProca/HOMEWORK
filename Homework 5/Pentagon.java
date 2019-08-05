0cbbbe68e5a1930b3fd30c5cc9d06f6a400c9257
RomanProca <romansermicioi@gmail.com.com>
2019-08-05 20:17:11 +0300

    -Modified Exercise for "Homework 5"

--- Homework 5/Pentagon.java
+++ Homework 5/Pentagon.java
@@ -3,7 +3,7 @@ class Pentagon {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the length from center to a vertex:");
-        double r = input.nextDouble();
+        Double r = input.nextDouble();
         double side = (2 * r) * (Math.sin(Math.PI/5));
         double area = (5 * Math.pow(side, 2)) / (4 * Math.tan(Math.PI /5));
         System.out.printf("The area of pentagon is %.2f\n", area);
